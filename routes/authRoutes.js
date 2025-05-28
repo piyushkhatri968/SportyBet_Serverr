225407f728777d61d34ab902ce3e47601846f473
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-28 12:09:28 +0500

    /

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -267,7 +267,7 @@ router.get("/admin/getAllUsers", async (req, res) => {
     res.status(200).json({ success: true, allUsers });
   } catch (error) {
     console.error("Error fetching all users", error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Server error", errorr: error });
   }
 });
 
