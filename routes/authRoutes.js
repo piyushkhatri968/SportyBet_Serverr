50ce6b00818bd77036dde45cdfb6d4768aedcad6
Jagdeshkumar422 <jk9876211@gmail.com>
2025-06-17 20:26:33 +0500

    dffgf

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -411,4 +411,18 @@ router.put("/admin/activeUserAccount/:id", async (req, res) => {
   }
 });
 
+router.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({ user });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 module.exports = router;
