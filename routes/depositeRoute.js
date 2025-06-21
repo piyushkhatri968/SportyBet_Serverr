564ec152d462d6e6629d79573ada7e0f9c86a0bd
Jagdeshkumar422 <jk9876211@gmail.com>
2025-06-21 07:51:47 +0500

    dffgf

--- routes/depositeRoute.js
+++ routes/depositeRoute.js
@@ -4,11 +4,6 @@ const Deposit = require("../models/deposite");
 const User = require("../models/user"); // ensure you have the User model
 const twilio = require("twilio");
 
-// Twilio setup (Make sure your .env file contains these variables)
-const accountSid = "AC1730b0fc10d26fef699a1aeade079cad";
-const authToken = "64d43bcb806298df54b5cf41e1eae438";
-const fromNumber = "+1 620 531 9469";
-const client = twilio(accountSid, authToken);
 
 // ============================
 // @route   POST /api/deposits/deposit
@@ -71,15 +66,7 @@ router.post("/withdraw", async (req, res) => {
     await deposit.save();
 
     // Send SMS notification
-    const user = await User.findById(userId);
-    if (user && user.mobileNumber) {
-      console.log(user.mobileNumber)
-      await client.messages.create({
-        body: `✅ Your withdrawal of ${amount} GHS was successful. Remaining balance: ${deposit.amount} GHS.`,
-        from: fromNumber,
-        to: user.phone, // must be in +233XXXXXXXX format
-      });
-    }
+
 
     res.status(200).json({ message: "Withdrawal successful", deposit });
   } catch (error) {
