72d48f6dd0edfcae200948747f17ca94d1ffc1cd
Jagdeshkumar422 <jk9876211@gmail.com>
2025-06-23 17:09:13 +0500

    dffgf

--- models/UserBalance.js
+++ models/UserBalance.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserBalanceSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
   amount: { type: Number, default: 0 },
-  currencyType: { type: String, enum: ["GHS", "NGN"], default: "NGN" },
+  currencyType: { type: String, enum: ["GHS", "NGN"], default: "GHS" },
 });
 
 module.exports = mongoose.model("UserBalance", UserBalanceSchema);
