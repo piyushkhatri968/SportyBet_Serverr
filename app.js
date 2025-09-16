6031a03b13977ff154ccf366547f316359f0fc5e
Piyush Khatri <piyushkhatri968@gmail.com>
2025-09-16 20:55:34 +0500

    .

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(
   cors({
-    origin: "https://admingh.online",
+    origin: [
+      "https://admingh.online",
+      "https://www.admingh.online",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
