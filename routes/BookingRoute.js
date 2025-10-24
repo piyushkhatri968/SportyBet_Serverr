5d3af35f58b60ded58acda366b7a955c49bad472
piyushkhatri968 <piyushkhatri968@gmail.com>
2025-10-24 12:01:16 +0500

    correct the date formate

--- routes/BookingRoute.js
+++ routes/BookingRoute.js
@@ -17,7 +17,7 @@ const formatDate = (date) => {
   const hours = now.getHours().toString().padStart(2, "0");
   const minutes = now.getMinutes().toString().padStart(2, "0");
 
-  return `${month}/${day}, ${hours}:${minutes}`;
+  return `${day}/${month}, ${hours}:${minutes}`;
 };
 ;
 
